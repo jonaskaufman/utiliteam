fedd99566f390406239f805c47d035b100657b93
Jonas Kaufman <jonas.l.kaufman@gmail.com>
2020-02-13 17:45:17 -0800

    Implemented stack

--- assignments/stack_implementation/stack.hpp
+++ assignments/stack_implementation/stack.hpp
@@ -1,21 +1,26 @@
+#include <vector>
+
 class IntStack
 {
     public:
 
         /// Returns true if there are no elements on the stack
-        bool empty() const;
+        bool empty() const { return _container.empty(); };
 
         /// Returns the number of elements on the stack
-        int size() const;
+        int size() const { return _container.size(); };
 
         /// Return the top element of the stack, i.e. the last element that was pushed
-        int top() const;
+        int top() const { return _container.back(); };
 
         /// Adds a new element to the top of the stack
-        void push(const int& new_value);
+        void push(const int& new_value) { _container.emplace_back(new_value); };
 
         /// Remove the topmost element of the stack
-        void pop();
+        void pop() { _container.pop_back(); };
 
     private:
+
+        /// The elements of the stack, stored in a vector
+        std::vector<int> _container;
 };
